dca76230a0230f5f507f19217c0019e2a3f44218
刘鑫伟 <liuxinwei1995@gmail.com>
2016-07-23 22:14:18 +0800

    最新数据库。

--- WebRoot/resource/sql/onlinebanking.sql
+++ WebRoot/resource/sql/onlinebanking.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50621
 File Encoding         : 65001
 
-Date: 2016-07-22 14:56:33
+Date: 2016-07-23 22:13:38
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -66,11 +66,12 @@ CREATE TABLE `client` (
   `UserPassword` varchar(10) NOT NULL,
   PRIMARY KEY (`ClientId`),
   UNIQUE KEY `IdentityId` (`IdentityId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of client
 -- ----------------------------
+INSERT INTO `client` VALUES ('1', '0', 'xiaohejun', 'man', '22222222', '1015206533@qq.com', '1', '20131925', '20131925');
 
 -- ----------------------------
 -- Table structure for `clientaccount`
@@ -87,4 +88,58 @@ CREATE TABLE `clientaccount` (
 
 -- ----------------------------
 -- Records of clientaccount
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `clientsubmitinfo`
+-- ----------------------------
+DROP TABLE IF EXISTS `clientsubmitinfo`;
+CREATE TABLE `clientsubmitinfo` (
+  `infoId` int(11) NOT NULL AUTO_INCREMENT,
+  `infoType` int(11) NOT NULL,
+  `infoContent` varchar(256) DEFAULT NULL,
+  `clientId` int(11) NOT NULL,
+  PRIMARY KEY (`infoId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of clientsubmitinfo
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `creditcard`
+-- ----------------------------
+DROP TABLE IF EXISTS `creditcard`;
+CREATE TABLE `creditcard` (
+  `cid` int(20) NOT NULL DEFAULT '0',
+  `spassword` varchar(16) DEFAULT NULL,
+  `cpassword` int(6) DEFAULT NULL,
+  `amount` int(15) DEFAULT NULL,
+  `integrate` int(10) DEFAULT NULL,
+  `isloss` tinyint(1) DEFAULT NULL,
+  `isactive` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`cid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of creditcard
+-- ----------------------------
+INSERT INTO `creditcard` VALUES ('123', '456', null, null, null, null, null);
+INSERT INTO `creditcard` VALUES ('234', '456', null, null, null, null, null);
+INSERT INTO `creditcard` VALUES ('123456', '123456', '789789', '5000', '500', '0', '1');
+
+-- ----------------------------
+-- Table structure for `websiteinfo`
+-- ----------------------------
+DROP TABLE IF EXISTS `websiteinfo`;
+CREATE TABLE `websiteinfo` (
+  `websiteId` int(11) NOT NULL AUTO_INCREMENT,
+  `buildTime` date NOT NULL,
+  `websiteAddr` varchar(64) NOT NULL,
+  `websiteName` varchar(64) NOT NULL,
+  PRIMARY KEY (`websiteId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of websiteinfo
 -- ----------------------------
